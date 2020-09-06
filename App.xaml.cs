6fb34e5c09c1be65ffc8a9fb70288de5ee5267d8
Marmex Mander <vadim.m.lutsk@gmail.com>
2020-09-06 17:53:22 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AlexGyver_s_Lamp_Control_Panel
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
